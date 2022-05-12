a9cf1c3a9c88c860992179d99d750bcc972eb96d
JB Aubrée <jb.aubree@digitsole.com>
2022-05-12 10:43:49 +0200

    first commit

---
+++ vite.config.ts
@@ -0,0 +1,29 @@
+import { resolve } from 'path'
+import { defineConfig } from 'vite'
+import Vue from '@vitejs/plugin-vue'
+import Components from 'unplugin-vue-components/vite'
+import AutoImport from 'unplugin-auto-import/vite'
+import Unocss from 'unocss/vite'
+
+export default defineConfig({
+  resolve: {
+    alias: {
+      '~/': `${resolve(__dirname, 'src')}/`,
+    },
+  },
+  plugins: [
+    Vue({ reactivityTransform: true }),
+    AutoImport({
+      imports: [
+        'vue',
+        'vue/macros',
+        '@vueuse/core',
+      ],
+      dts: true,
+    }),
+    Components({
+      dts: true,
+    }),
+    Unocss(),
+  ],
+})
