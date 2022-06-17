263ee1658c93bd7202bfac13b46526ad74adcf7e
JB Aubrée <jb.aubree@digitsole.com>
2022-06-17 13:38:20 +0200

    fix: use mouse scroll only on top

--- src/components/ScrollMouse.vue
+++ src/components/ScrollMouse.vue
@@ -1,10 +1,8 @@
 <script setup lang="ts">
-const { pages } = useScreen()
 const { isScrolling, y } = useScroll(window)
 const { isMdAndLarger } = useTailwindBreakpoints()
 
 let showScrollIcon = $ref(false)
-
 let timer: ReturnType<typeof setTimeout> | undefined
 
 watch(isScrolling, (value) => {
@@ -12,28 +10,22 @@ watch(isScrolling, (value) => {
     clearTimeout(timer)
     showScrollIcon = false
   }
-  else if (pages.value.find(page => page.offsetTop === y.value)) {
+  else if (y.value === 0) {
     timer = setTimeout(() => {
       showScrollIcon = true
     }, 3000)
   }
 }, { immediate: true })
-
-const isLightMouse = $computed(() => {
-  return pages.value.find(page => page.offsetTop === y.value)?.className.includes('bg-primary')
-})
 </script>
 
 <template>
   <Transition>
     <div v-if="showScrollIcon && isMdAndLarger" class="z-100 fixed bottom-10 left-50% translate-x--50%">
       <span
-        class="block relative h-3.25rem w-2rem rounded-1rem border border-.22rem"
-        :class="isLightMouse ? 'border-white' : 'border-secondary'"
+        class="block relative h-3.25rem w-2rem rounded-1rem border border-.22rem border-white"
       >
         <span
-          class="animate-dot block absolute left-50% h-.5rem w-.5rem top-.6rem ml--.25rem rounded-full backface-hidden"
-          :class="isLightMouse ? 'bg-white' : 'bg-secondary'"
+          class="animate-dot block absolute left-50% h-.5rem w-.5rem top-.6rem ml--.25rem rounded-full backface-hidden bg-white"
         />
       </span>
     </div>
