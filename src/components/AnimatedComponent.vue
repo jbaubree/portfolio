7bb9232799ef6ef37d0759a46791f0d89d576fda
JB Aubrée <jb.aubree@digitsole.com>
2022-06-17 11:24:31 +0200

    refactor: unscop fade animations

--- src/components/AnimatedComponent.vue
+++ src/components/AnimatedComponent.vue
@@ -33,39 +33,10 @@ onMounted(() => {
 
 <template>
   <div ref="target">
-    <transition :name="animationType">
+    <Transition :name="animationType">
       <div v-appear="animate" class="animated-component">
         <slot />
       </div>
-    </transition>
+    </Transition>
   </div>
 </template>
-
-<style scoped>
-.animated-component.fade-enter-from,
-.animated-component.zoom-enter-from {
-  transition: none;
-}
-
-/* Fade animation */
-.fade-enter-active,
-.fade-leave-active {
-  transition: opacity 300ms ease;
-}
-
-.fade-enter-from,
-.fade-leave-to {
-  opacity: 0;
-}
-
-/* Zoom animation */
-.zoom-enter-active,
-.zoom-leave-active {
-  transition: transform 300ms ease;
-}
-
-.zoom-enter-from,
-.zoom-leave-to {
-  transform: scale(0.9);
-}
-</style>
