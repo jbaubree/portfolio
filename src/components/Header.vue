0399b98bc72eeb9d7cf53e676dfcbf0141fe9c9f
JB Aubrée <jb.aubree@digitsole.com>
2022-06-22 14:57:13 +0200

    style: update header blur and bg opacity

--- src/components/Header.vue
+++ src/components/Header.vue
@@ -24,7 +24,7 @@ watch(isScrolling, (value) => {
 <template>
   <div
     id="header"
-    class="w-full z-5 px-10 fixed flex justify-center bg-#031994 bg-opacity-60 rounded-b-5 backdrop-blur-4 transition-transform duration-500"
+    class="w-full z-5 px-10 fixed flex justify-center bg-#031994 bg-opacity-70 rounded-b-5 backdrop-blur-6 transition-transform duration-500"
     :style="showHeader ? 'transform: translateY(0)' : `transform: translateY(${-headerHeight}px)`"
   >
     <Container
