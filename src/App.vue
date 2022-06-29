7cc8473c39a80359cb96f5bfc29c03337ab32c5f
JB Aubrée <jb.aubree@digitsole.com>
2022-06-29 09:34:52 +0200

    fix: put footer under content to get better gradient on themed view

--- src/App.vue
+++ src/App.vue
@@ -6,21 +6,23 @@ const { isMdAndLarger } = useTailwindBreakpoints()
 
 <template>
   <FadeInOut :duration="300" mode="out-in">
-    <div v-if="fadeDark" class="m-0 p-0 overflow-hidden" :class="{ 'bg-primary': !isDark }" :style="isDark ? gradientStyle : ''">
-      <Header />
-      <div
-        class="transition-all bg-transparent"
-        :style="`margin-top: ${isMdAndLarger ? '0px' : '20px'};`"
-      >
-        <SectionHero />
-        <SectionProfile />
-        <SectionServices />
-        <SectionProjects />
-        <SectionStack />
-        <SectionContact />
-        <ScrollTop />
-        <ScrollMouse />
-        <Notification />
+    <div style="cursor: url('data:image/svg+xml,%3csvg xmlns='http://www.w3.org/2000/svg' width='32' height='32' ...') 16 16, pointer;">
+      <div v-if="fadeDark" class="m-0 p-0 overflow-hidden" :class="{ 'bg-primary': !isDark }" :style="isDark ? gradientStyle : ''">
+        <Header />
+        <div
+          class="transition-all bg-transparent"
+          :style="`margin-top: ${isMdAndLarger ? '0px' : '20px'};`"
+        >
+          <SectionHero />
+          <SectionProfile />
+          <SectionServices />
+          <SectionProjects />
+          <SectionStack />
+          <SectionContact />
+          <ScrollTop />
+          <ScrollMouse />
+          <Notification />
+        </div>
       </div>
       <Footer />
     </div>
